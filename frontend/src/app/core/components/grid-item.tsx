cf5b897afcf14906b17018053e5bd092fd813ac1
Morel Sébastien <morel.seb@gmail.com>
2022-05-17 14:27:54 -0700

    Quick clean

--- frontend/src/app/core/components/grid-item.tsx
+++ frontend/src/app/core/components/grid-item.tsx
@@ -3,10 +3,7 @@ import { Embed } from './embed';
 import { Slider } from './slider';
 
 export const GridItem = ({ cell }: { cell: any }) => {
-    let colspan = cell.layout.colspan;
     let view = cell.item.components.find((component: any) => component.id === 'view')?.content?.options?.[0]?.value;
-    let isFullWidth = cell.item.components.find((component: any) => component.id === 'fullwidth-tile')?.content?.value;
-
     return (
         <div>
             {view === 'banner' && <Banner cell={cell} />}

ac988792b08432352e7bb8f81a37ab5411b79773
Morel Sébastien <morel.seb@gmail.com>
2022-05-25 18:37:31 -0600

    :mushroom: Fix Console errors

--- frontend/src/app/core/components/basket-button.tsx
+++ frontend/src/app/core/components/basket-button.tsx
@@ -2,21 +2,24 @@ import { useRemoteCart } from '../hooks/useRemoteCart';
 import BasketIcon from '~/assets/basketIcon.svg';
 import { Link } from '@remix-run/react';
 import { useLocalCart } from '../hooks/useLocalCart';
+import { ClientOnly } from '@crystallize/reactjs-hooks';
 
 export const BasketButton = ({}) => {
     const { cart, isEmpty } = useLocalCart();
     let quantity = 0;
     if (!isEmpty()) {
-        quantity = Object.keys(cart.items).reduce((acc: number, item: any) => acc + item.quantity, 0);
+        quantity = Object.keys(cart.items).reduce((acc: number, key: string) => acc + cart.items[key].quantity, 0);
     }
     return (
         <div className="relative">
             <Link to="/cart">
                 <img src={`${BasketIcon}`} />
             </Link>
-            <div className="absolute -top-2 -right-2 rounded-full bg-textBlack w-4 h-4  text-[#fff] text-center text-[12px]">
-                {quantity}
-            </div>
+            <ClientOnly>
+                <div className="absolute -top-2 -right-2 rounded-full bg-textBlack w-4 h-4  text-[#fff] text-center text-[12px]">
+                    {quantity}
+                </div>
+            </ClientOnly>
         </div>
     );
 };

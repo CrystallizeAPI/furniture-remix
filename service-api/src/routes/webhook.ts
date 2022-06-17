25cf5bb20f6286eaf63fd89503cd09d19e56be14
Morel Sébastien <morel.seb@gmail.com>
2022-06-16 21:34:39 -0700

    :alien: Webhook to purge Fastly

--- service-api/src/routes/webhook.ts
+++ service-api/src/routes/webhook.ts
@@ -1,5 +1,6 @@
 import { StandardRouting } from '@crystallize/node-service-api-router';
 import Koa from 'koa';
+import fetch from 'node-fetch';
 
 export const webhookStandardRoutes: StandardRouting = {
     '/webhook/order/created': {
@@ -18,6 +19,30 @@ export const webhookStandardRoutes: StandardRouting = {
                 ctx.response.body = {
                     message: 'Order Pipleline Stage Change Webhook received',
                     payload: ctx.request.body,
+                };
+            },
+        },
+    },
+    '/webhook/cache/purge': {
+        post: {
+            handler: async (ctx: Koa.Context) => {
+                const payload = ctx.request.body;
+                // we keep it simple for now and we purge all cache for the tenant identifier
+                const keys = [ctx.storeFront.config.tenantIdentifier];
+                const response = await fetch(`https://api.fastly.com/service/${process.env.FASTLY_SERVICE_ID}/purge`, {
+                    method: 'POST',
+                    headers: {
+                        'fastly-soft-purge': `1`,
+                        'Fastly-Key': `${process.env.FASTLY_SERVICE_KEY}`,
+                        'Content-Type': 'application/json',
+                        Accept: 'application/json',
+                        'surrogate-key': keys.join(' '),
+                    },
+                });
+                const keyPurged = await response.json();
+                ctx.response.body = {
+                    message: `${Object.keys(keyPurged).length} key(s) soft purged.`,
+                    keys: keyPurged,
                 };
             },
         },

9fc00f513128ca16d1332fed9ec55b977fb05337
didrikhegna <didrik@snowball.digital>
2022-06-20 16:01:19 +0200

    fixed mobile menu sizes ++

--- frontend/src/app/core/components/topic-navigation.tsx
+++ frontend/src/app/core/components/topic-navigation.tsx
@@ -4,8 +4,9 @@ export const TopicNavigation: React.FC<{ navigation: any }> = ({ navigation }) =
     return (
         <>
             {navigation?.tree?.map((topic: any) => (
-                <span className="nav-anchor" key={topic.path}>
-                    <span>{topic.name}</span> <span className="text-[0.6em] caret">▼</span>
+                <span className="nav-anchor " key={topic.path}>
+                    <span className="text-xl lg:text-[16px] block hover:underline">{topic.name}</span>
+                    <span className="hidden lg:flex text-[0.6em] caret">▼</span>
                     <ul className="nav-dropdown">
                         {topic.children?.map((child: any) => (
                             <Link to={child.path} prefetch="intent" key={child.path}>

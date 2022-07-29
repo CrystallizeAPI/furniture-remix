918a16c07bdf80e526e1ca98255218a5da1ca48f
Raymond Julin <raymond.julin@gmail.com>
2022-07-29 10:07:17 +0200

    feat: Support setting SUPERFAST_HOST to override the provided hostname to superfast tenant fetch

--- application/environment.d.ts
+++ application/environment.d.ts
@@ -1,13 +1,14 @@
 declare global {
-  namespace NodeJS {
-    interface ProcessEnv {
-      REDIS_DSN?: string
-      STOREFRONT_CONFIG_FILE?: string
-      SUPERFAST_TENANT_IDENTIFIER: string
-      SUPERFAST_ACCESS_TOKEN_ID: string
-      SUPERFAST_ACCES_TOKEN_SECRET: string
+    namespace NodeJS {
+        interface ProcessEnv {
+            SUPERFAST_HOST?: string;
+            REDIS_DSN?: string;
+            STOREFRONT_CONFIG_FILE?: string;
+            SUPERFAST_TENANT_IDENTIFIER: string;
+            SUPERFAST_ACCESS_TOKEN_ID: string;
+            SUPERFAST_ACCES_TOKEN_SECRET: string;
+        }
     }
-  }
 }
 
-export {}
+export {};

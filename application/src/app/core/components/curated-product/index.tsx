ca70c0a2dd4baa572f222717e1e9f5931cbdc562
didrikhegna <didrik@snowball.digital>
2022-08-18 14:12:27 +0200

    added qty input to currated products

--- application/src/app/core/components/curated-product/index.tsx
+++ application/src/app/core/components/curated-product/index.tsx
@@ -16,6 +16,7 @@ export function CuratedProduct({ merch, current }: { merch: any; current: any })
 
 const Product = ({ product, current }: { product: any; current: any }) => {
     const primaryVariant = product.variants.find((v: any) => v.isDefault);
+    const [qty, setQtyState] = useState(1);
     let [selectedVariant, setSelectedVariant] = useState(primaryVariant);
     let location = useLocation();
 
@@ -37,6 +38,13 @@ const Product = ({ product, current }: { product: any; current: any }) => {
         current.setVariants((prevState: any) => [...prevState, primaryVariant]);
     }, [location.pathname]);
 
+    const setQty = (qty) => {
+        if (qty < 0) {
+            return;
+        }
+        setQtyState(qty);
+    };
+
     return (
         <>
             <div className="flex gap-2 justify-between flex-wrap items-center py-2">
@@ -56,6 +64,25 @@ const Product = ({ product, current }: { product: any; current: any }) => {
                         onVariantChange={onVariantChange}
                         renderingType="dropdown"
                     />
+                    <div className="flex flex-col">
+                        <span className="block text-xs pb-1 font-medium">Quantity</span>
+                        <div>
+                            <button
+                                className="py-2 px-4 bg-[#fff] text-sm hover:bg-grey-400"
+                                onClick={() => setQty(qty - 1)}
+                            >
+                                -
+                            </button>
+                            <input
+                                value={qty}
+                                className="py-2  px-4 bg-[#fff] text-sm w-[60px] text-center "
+                                onChange={(e) => setQty(e.target.value)}
+                            />
+                            <button className="py-2 px-4 bg-[#fff] text-sm " onClick={() => setQty(qty + 1)}>
+                                +
+                            </button>
+                        </div>
+                    </div>
                 </div>
             </div>
         </>
